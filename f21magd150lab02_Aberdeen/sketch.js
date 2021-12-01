8c2994413b8e1174d4c33a033d9d7f3e09ba3192
alyxcat <90303587+alyxcat@users.noreply.github.com>
2021-12-01 12:46:59 -0600

    Add files via upload

---
+++ f21magd150lab02_Aberdeen/sketch.js
@@ -0,0 +1,150 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  colorMode(HSB);
+background(289, 7, 90);
+  
+  
+  noStroke();
+  colorMode(RGB)
+  fill(236, 24, 236)
+ circle(100, 210, 140);
+  
+  
+  noStroke();
+  colorMode(RGB)
+  fill(246, 146, 211)
+  circle (300,100,120)
+  
+  noStroke()
+  colorMode(RGB)
+  fill(47, 238, 54)
+  arc(300, 100, 120, 120, 0, PI + QUARTER_PI, OPEN);
+  
+    
+  
+   noFill();
+ colorMode(RGB);
+  strokeWeight(15);
+  stroke(246, 146, 211)
+bezier(320, 54, 290, 40, 240, 170, 270, 100);
+  
+  noFill();
+ colorMode(RGB);
+  strokeWeight(15);
+  stroke(246, 146, 211)
+bezier(320, 66, 380, 40, 280, 170, 305, 140);
+  
+  
+  
+  noFill();
+ colorMode(RGB);
+  strokeWeight(15);
+  stroke(246, 146, 211)
+bezier(320, 75, 340, 5, 270, 170, 300, 100);
+  
+  
+  
+  
+  
+  
+  
+  
+  noStroke();
+  colorMode(RGB)
+  fill(80, 121, 241)
+  circle (270,265,100)
+  
+  noFill();
+ colorMode(RGB);
+  strokeWeight(15);
+  stroke(120,0,193)
+bezier(62, 160, 167, 181, 20, 240, 156, 235);
+  
+    noFill();
+ colorMode(RGB);
+  strokeWeight(15);
+  stroke(110,16,193)
+bezier(105, 148, 150, 160, 100, 200, 160, 200);
+  
+  
+  
+    noFill();
+ colorMode(RGB);
+  strokeWeight(15);
+  stroke(110,16,193)
+bezier(40, 190, 100, 180, 30, 300, 130, 265);
+  
+  
+  
+  
+  
+  
+  
+  
+
+ stroke('#7bf4a9');
+strokeWeight(10);
+bezier(217, 280, 140, 323, 396, 267, 321, 250);
+
+   stroke('#47f0f0');
+strokeWeight(10);
+bezier(230, 304, 187, 362, 400, 210, 300, 219);
+
+  
+  
+  
+  
+  
+  
+  
+  //points
+ 
+  beginShape(POINTS);
+  stroke('#47f0r7');
+strokeWeight(5);
+vertex(30, 29);
+vertex(105, 40);
+vertex(55, 75);
+  vertex(25, 62);
+vertex(15, 335);
+  vertex(365, 75);
+  vertex(205, 45);
+  vertex(223, 82);
+  vertex(189, 69);
+  vertex(158, 102);
+  vertex(172, 27);
+  vertex(25, 129);
+  vertex(15, 144);
+  vertex(6, 92);
+  vertex(205, 143);
+  vertex(305, 190);
+  vertex(333, 177);
+vertex(380, 90);
+vertex(300,30);
+vertex(180,300);
+  vertex(270,390);
+  vertex(254,322);
+  vertex(221,378);
+  
+  vertex(234,210);
+  vertex(230,200);
+  vertex(300,340);
+  vertex (123,340);
+    vertex(360,345);
+    vertex(390,389);
+    vertex(270,356);
+    vertex(90,300);
+    vertex(85,288);
+    vertex(98,377);
+endShape();
+  
+}
+
+ 
+
+
+
