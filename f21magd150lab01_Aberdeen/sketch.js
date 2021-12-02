9c6b327f4c6d7c7baa4466d7a5fe02c059b6196b
alyxcat <90303587+alyxcat@users.noreply.github.com>
2021-12-01 18:30:04 -0600

    Add files via upload

---
+++ f21magd150lab01_Aberdeen/sketch.js
@@ -0,0 +1,209 @@
+function setup() {
+  createCanvas(425, 400);
+}
+function draw() {
+  background(55);
+  
+  ellipseMode(CORNER)
+  fill(250);                
+noStroke();            
+ellipse(330,40,40,40);
+  
+  
+   fill(100);               
+stroke(250);
+  strokeWeight(5)
+  point(30, 20);
+  
+   fill(250);               
+stroke(250);
+  strokeWeight(2)
+  point(40, 50);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(2)
+  point(40, 50);
+  
+    fill(250);               
+stroke(250);
+  strokeWeight(3)
+  point(90, 60);
+  
+      fill(250);               
+stroke(250);
+  strokeWeight(5)
+  point(150, 80);
+  
+  
+      fill(250);               
+stroke(250);
+  strokeWeight(1)
+  point(300, 35);
+  
+     fill(250);               
+stroke(250);
+  strokeWeight(1)
+  point(340, 20);
+  
+  
+     fill(250);               
+stroke(250);
+  strokeWeight(3)
+  point(370, 20);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(3)
+  point(310, 20);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(3)
+  point(280, 60);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(2)
+  point(280, 10);
+  
+    fill(250);               
+stroke(250);
+  strokeWeight(1)
+  point(80, 10);
+  
+    fill(250);               
+stroke(250);
+  strokeWeight(2)
+  point(100, 10);
+  
+   fill(250);               
+stroke(250);
+  strokeWeight(2)
+  point(50, 30);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(1)
+  point(50, 70);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(1)
+  point(40, 100);
+  
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(4)
+  point(60, 130);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(1)
+  point(80, 160);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(3)
+  point(20, 140);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(3)
+  point(20, 77);
+  
+   fill(250);               
+stroke(250);
+  strokeWeight(3)
+  point(180, 77);
+  
+   fill(250);               
+stroke(250);
+  strokeWeight(2)
+  point(190, 68);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(2)
+  point(146, 48);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(1)
+  point(213, 36);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(3)
+  point(235, 150);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(2)
+  point(205, 170);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(1)
+  point(190, 130);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(5)
+  point(360, 120);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(1)
+  point(360, 150);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(2)
+  point(390, 190);
+  
+  fill(250);               
+stroke(250);
+  strokeWeight(1)
+  point(390, 100);
+  
+  
+  fill(100);               
+stroke(250);
+  rect(15, 200, 75, 205, 10);
+  fill(240);               
+stroke(150);
+    rect(95, 100, 75, 305, 10);
+  fill(100);               
+stroke(250);
+    rect(175, 200, 75, 205, 10);
+   fill(240);               
+stroke(150);
+   rect(255, 100, 75, 305, 10);
+   fill(100);               
+stroke(250);
+  rect(335, 200, 75, 205, 10);
+  
+  
+ 
+   fill(50);               
+stroke(130);
+  rect(65, 300, 55, 105, 10);
+  
+  
+    fill(50);               
+stroke(130);
+  rect(145, 300, 55, 105, 10);
+
+  
+    fill(50);               
+stroke(130);
+  rect(225, 300, 55, 105, 10);
+  
+  
+  
+   fill(50);               
+stroke(130);
+  rect(305, 300, 55, 105, 10);
+}
